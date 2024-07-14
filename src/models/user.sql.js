3d05f514e6b0d89579e6dca11f81bf612f2cf537
lee gahyun <158866109+egahyun@users.noreply.github.com>
2024-07-15 00:21:36 +0900

    Add : 초기 폴더 구조 및 파일 구성
    plant_record, plant, soulmate, user를 기준으로 파일 구성
    swagger 제외하고 일부 파일 코드 구현

---
+++ src/models/user.sql.js
@@ -0,0 +1,1 @@
+export const insertUserSql = "INSERT INTO USER (nickname, member_id, password, phonenum, marketing_agree, profile_img) VALUES (?, ?, ?, ?, ?, ?);";
---
+++ src/routes/user.route.js
@@ -0,0 +1,3 @@
+// routes/user.route.js
+import express from "express";
+import asyncHandler from 'express-async-handler';
