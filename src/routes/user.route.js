bb08a8144619e3887006baac6881ac07696321a4
박예인 <yenqkr@MacBook-Pro.local>
2024-07-17 23:00:52 +0900

    feat:테스트용 userRoute 추가

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,3 +1,7 @@
 // routes/user.route.js
 import express from "express";
-import asyncHandler from 'express-async-handler';
+import { userSignin } from "../controllers/user.controller.js";
+
+export const userRouter = express.Router();
+
+userRouter.post('/signin', userSignin);
